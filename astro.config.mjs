6fb976b8cac88c099c213d15c9c863dbabe67a06
Frakits <90983918+Frakits@users.noreply.github.com>
2026-08-28 23:06:59 +0300

    here's my super genius idea

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import alpine from '@astrojs/alpinejs';
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+	outDir: "./dist/cne-web-test",
 	markdown: {
 		smartypants: false,
 		syntaxHighlight: 'prism',
